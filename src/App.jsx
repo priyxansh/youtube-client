ca906080844c780059727a3f84bbc9c6603d62b3
Priyansh <priyanshrai16@gmail.com>
2023-03-25 00:02:07 +0530

    Laid out the basic structure for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import NavBar from "./components/NavBar/NavBar";
+import Feed from "./components/Feed/Feed";
+import SearchFeed from "./components/SearchFeed/SearchFeed";
+import VideoDetails from "./components/VideoDetails/VideoDetails";
+import ChannelDetails from "./components/ChannelDetails/ChannelDetails";
 
 function App() {
-
-  return null;
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetails />} />
+                <Route path="/channel/:id" element={<ChannelDetails />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
